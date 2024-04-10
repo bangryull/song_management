632db0fa46202985b0f3ac114c4115dc746586d4
sonwh123 <80819521+sonwh123@users.noreply.github.com>
2024-04-10 15:10:00 +0900

    Update song.c

--- code/song.c
+++ code/song.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
+#include <time.h>
 
 #define STRING_SIZE 256
 
+struct Artist { //가수, 작곡가, 작사가 -> 아티스트로 묶기
+    char name[STRING_SIZE];     //아티스트 이름
+    clock_t Key;                //Key값(시간)
+};
+
 struct Song { //구조체
     char title[STRING_SIZE];    //제목
-    char singer[STRING_SIZE];   //가수
-    char composer[STRING_SIZE]; //작곡가
-    char lyricist[STRING_SIZE]; //작사가
+    struct Artist singer;       //가수
+    struct Artist composer;     //작곡가
+    struct Artist lyricist;     //작사가
     char genre[STRING_SIZE];    //장르
     char playtime[STRING_SIZE]; //재생시간
     char album[STRING_SIZE];    //앨범명
     char release[STRING_SIZE];  //앨범출시날짜
-
 };
 
-int main(){
+int main() {
     int mode;
     printf("menu\n");
     printf("1. 노래 리스트 관리\n");
@@ -22,27 +27,27 @@ int main(){
     printf("3. 플레이리스트\n");
     printf("4. 종료\n");
     printf("메뉴 선택 : ");
-    scanf("%d",&mode);
+    scanf_s("%d", &mode);
 
-    switch (mode){
-        case 1: //노래 리스트 관리
-            
-            break;
-        
-        case 2: //검색 기능
+    switch (mode) {
+    case 1: //노래 리스트 관리
 
-            break;
+        break;
 
-        case 3: //플레이리스트
+    case 2: //검색 기능
 
-            break;
+        break;
 
-        case 4: //종료
+    case 3: //플레이리스트
 
-            break;
+        break;
 
-        default: //error
-            break;
+    case 4: //종료
+
+        break;
+
+    default: //error
+        break;
     }
     return 0;
 }
